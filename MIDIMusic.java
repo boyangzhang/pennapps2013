c0742aca42eb73d71f0346efac546c781ce07aa0
Dan Zhang <danzhang@seas.1231.apng.wireless-pennnet.upenn.edu>
2013-09-07 01:27:40 -0400

    basic midi api

---
+++ MIDIMusic.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.midi.*;
+
+public class MIDIMusic {
+
+	Sequence  sequence;
+	Sequencer sequencer;
+	Synthesizer synthesizer;
+	
+	public MIDIMusic(String filename) throws InvalidMidiDataException, IOException, MidiUnavailableException {
+		sequence    = MidiSystem.getSequence(new File("tnfdm.mid"));
+		sequencer   = MidiSystem.getSequencer(true);
+		sequencer.setSequence(sequence);
+		synthesizer = MidiSystem.getSynthesizer();
+	}
+	
+	public void play() {
+		sequencer.start();
+	}
+	
+	public void pause() {
+		sequencer.stop();
+	}
+	
+	public void setSpeed(float scalar) {
+		sequencer.setTempoFactor(scalar);
+	}
+	
+}
